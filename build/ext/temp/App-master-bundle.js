624450632aa84b6e1c1b0f7f01aeec8b535ff6c4
madugulaharish08@gmail.com <madugulaharish08@gmail.com>
2019-08-09 22:35:17 +0530

    Kitchensink

---
+++ build/ext/temp/App-master-bundle.js
@@ -0,0 +1,3 @@
+// @tag App
+// @require /home/miracle/Hk/ExtJS/ext-web-components/packages/ext-web-components-kitchensink/node_modules/@sencha/ext-froala-editor/froalaeditor.pkgd.js
+// @require /home/miracle/Hk/ExtJS/ext-web-components/packages/ext-web-components-kitchensink/build/ext/app.js
