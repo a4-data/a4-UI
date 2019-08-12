21474f46102d6004843fd6813dad09e38acb6a50
madugulaharish08@gmail.com <madugulaharish08@gmail.com>
2019-08-12 22:53:09 +0530

    Added REST API

--- src/view/main/MainComponent.js
+++ src/view/main/MainComponent.js
@@ -8,8 +8,231 @@ Ext.require([
     'Ext.data.TreeStore',
     'Ext.chart.series.Bar',
     'Ext.chart.axis.Numeric',
-    'Ext.chart.axis.Category'
+    'Ext.chart.axis.Category',
+    'Ext.data.*', 'Ext.grid.*'
 ]);
+
+Ext.define('User', {
+    extend: 'Ext.data.Model',
+    fields: ['id', 'name', 'year', 'pantone_value']
+});
+
+// var storee = Ext.create('Ext.data.Store', {
+//     model: 'User',
+//     proxy: {
+//         type: 'jsonp',
+//         url: 'https://reqres.in/api/products/3'
+//     }
+// });
+
+// storee.load();
+
+var storeee = Ext.create('Ext.data.Store', {
+    model: 'User',
+    proxy: {
+        type: 'rest',
+        url: 'https://reqres.in/api/products/3'
+    }
+});
+
+storeee.load(function (heck) {
+    console.log(heck[0].data.data)
+}); //GET /users
+// User.load({
+//     success: function (user) {
+//         console.log('i am here', user)
+//         console.log(user.getId()); //outputs 123
+//     }
+// });
+// Ext.define('Person', {
+//     extend: 'Ext.data.Model',
+//     fields: [{
+//         name: 'id',
+//         type: 'int',
+//         useNull: true
+//     }, 'email', 'first', 'last'],
+//     validations: [{
+//         type: 'length',
+//         field: 'email',
+//         min: 1
+//     }, {
+//         type: 'length',
+//         field: 'first',
+//         min: 1
+//     }, {
+//         type: 'length',
+//         field: 'last',
+//         min: 1
+//     }]
+// });
+
+// var MyApp = function () {
+//     return {
+//         init: function () {
+//             console.log('Init Called******************');
+//             var storeys = Ext.create('Ext.data.Store', {
+//                 autoLoad: true,
+//                 autoSync: true,
+//                 // model: 'Person',
+//                 proxy: {
+//                     type: 'rest',
+//                     // url: 'https://reqres.in/api/users',
+//                     url: 'https://reqres.in/api/products/3',
+//                     reader: {
+//                         type: 'json',
+//                         rootProperty: 'data'
+//                     },
+//                     writer: {
+//                         type: 'json'
+//                     }
+//                 },
+//                 listeners: {
+//                     write: function (storeys, operation) {
+//                         console.log('write_________________________');
+//                         console.log(operation, 'write_________________________');
+
+//                     }
+//                     // write: function (store, operation) {
+//                     //     var record = operation.getRecords()[0],
+//                     //         name = Ext.String.capitalize(operation.action),
+//                     //         verb;
+
+
+//                     //     if (name == 'Destroy') {
+//                     //         record = operation.records[0];
+//                     //         verb = 'Destroyed';
+//                     //     } else {
+//                     //         verb = name + 'd';
+//                     //     }
+//                     //     Ext.example.msg(name, Ext.String.format("{0} user: {1}", verb, record.getId()));
+
+//                     // }
+//                 }
+//             })
+//             // storeys.write();
+//             storeys.getId()
+//             console.log();
+//         }
+//     }
+// }();
+// Ext.onReady(MyApp.init, MyApp);
+
+// Ext.onReady(function () {
+
+//     var store = Ext.create('Ext.data.Store', {
+//         autoLoad: true,
+//         autoSync: true,
+//         model: 'Person',
+//         proxy: {
+//             type: 'rest',
+//             // url: 'https://reqres.in/api/users',
+//             url: 'https://reqres.in/api/products/3',
+//             reader: {
+//                 type: 'json',
+//                 root: 'data'
+//             },
+//             writer: {
+//                 type: 'json'
+//             }
+//         },
+//         listeners: {
+//             write: function (store, operation) {
+//                 var record = operation.getRecords()[0],
+//                     name = Ext.String.capitalize(operation.action),
+//                     verb;
+
+
+//                 if (name == 'Destroy') {
+//                     record = operation.records[0];
+//                     verb = 'Destroyed';
+//                 } else {
+//                     verb = name + 'd';
+//                 }
+//                 Ext.example.msg(name, Ext.String.format("{0} user: {1}", verb, record.getId()));
+
+//             }
+//         }
+//     });
+
+//     var rowEditing = Ext.create('Ext.grid.plugin.RowEditing', {
+//         listeners: {
+//             cancelEdit: function (rowEditing, context) {
+//                 // Canceling editing of a locally added, unsaved record: remove it
+//                 if (context.record.phantom) {
+//                     store.remove(context.record);
+//                 }
+//             }
+//         }
+//     });
+
+//     var grid = Ext.create('Ext.grid.Panel', {
+//         renderTo: document.body,
+//         plugins: [rowEditing],
+//         width: 400,
+//         height: 300,
+//         frame: true,
+//         title: 'Users',
+//         store: store,
+//         iconCls: 'icon-user',
+//         columns: [{
+//             text: 'ID',
+//             width: 40,
+//             sortable: true,
+//             dataIndex: 'id'
+//         }, {
+//             text: 'Email',
+//             flex: 1,
+//             sortable: true,
+//             dataIndex: 'email',
+//             field: {
+//                 xtype: 'textfield'
+//             }
+//         }, {
+//             header: 'First',
+//             width: 80,
+//             sortable: true,
+//             dataIndex: 'first',
+//             field: {
+//                 xtype: 'textfield'
+//             }
+//         }, {
+//             text: 'Last',
+//             width: 80,
+//             sortable: true,
+//             dataIndex: 'last',
+//             field: {
+//                 xtype: 'textfield'
+//             }
+//         }],
+//         dockedItems: [{
+//             xtype: 'toolbar',
+//             items: [{
+//                 text: 'Add',
+//                 iconCls: 'icon-add',
+//                 handler: function () {
+//                     // empty record
+//                     store.insert(0, new Person());
+//                     rowEditing.startEdit(0, 0);
+//                 }
+//             }, '-', {
+//                 itemId: 'delete',
+//                 text: 'Delete',
+//                 iconCls: 'icon-delete',
+//                 disabled: true,
+//                 handler: function () {
+//                     var selection = grid.getView().getSelectionModel().getSelection()[0];
+//                     if (selection) {
+//                         store.remove(selection);
+//                     }
+//                 }
+//             }]
+//         }]
+//     });
+//     grid.getSelectionModel().on('selectionchange', function (selModel, selections) {
+//         grid.down('#delete').setDisabled(selections.length === 0);
+//     });
+// });
+
 
 export default class MainComponent {
     constructor() {
